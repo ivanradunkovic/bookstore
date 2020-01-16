39338700995c1a2af4d877aaf2a81c845221e072
Ivan <ivan1501@gmail.com>
2020-01-16 11:37:35 +0100

    Added cart and books to reducers

---
+++ src/actions/booksActions.js
@@ -0,0 +1,30 @@
+// GET A BOOK
+export function getBooks(book) {
+    return {
+        type: "GET_BOOK", 
+    }
+};
+
+// POST A BOOK
+export function postBooks(book) {
+    return {
+        type: "POST_BOOK", 
+        payload: book
+    }
+};
+
+// DELETE A BOOK
+export function deleteBooks(title) {
+    return {
+        type: "DELETE_BOOK",
+        payload: title
+    }
+};
+
+// UPDATE A BOOK
+export function updateBooks(book) {
+    return {
+        type: "UPDATE_BOOK",
+        payload: book
+    }
+}
