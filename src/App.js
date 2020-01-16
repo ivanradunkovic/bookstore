dbf587683b071f177638a560a839383d06475898
Ivan <ivan1501@gmail.com>
2020-01-16 12:59:15 +0100

    Added view and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BooksList from './components/pages/booksList';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksFrom';
+import Main from './main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 
-export default App;
+import {Router, Route, IndexRoute, browserHistory, hashHistory} from 'react-router';
+
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+
+// IMPORT COMBINED REDUCERS
+import reducers from './reducers/index';
+// IMPORT ACTIONS
+import {addToCart} from './actions/cartActions';
+import {postBooks, deleteBooks, updateBooks} from './actions/booksActions'
+
+// STEP 1 create the store
+const middleware = applyMiddleware(logger);
+const store = createStore(reducers, middleware);
+
+
+
+const Routes = (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={BooksList}/>
+                <Route path="/admin" component={BooksForm}/>
+                <Route path="/cart" component={Cart}/>
+            </Route>
+        </Router>
+    </Provider>
+);
+
+render(
+    Routes, document.getElementById('app')
+);
+
+// POST a book
+// store.dispatch(postBooks(
+    
+// ));
+
+// // DELETE a book
+// store.dispatch(deleteBooks(
+//     {id: 1}
+// ));
+
+// // UPDATE a book
+// store.dispatch(updateBooks(
+//     {
+//         id: 2,
+//         title: 'Learn React in 24 hours'
+//     }
+// ));
+
+// // --> CART ACTION <--
+// store.dispatch(addToCart([{id: 1}]));
